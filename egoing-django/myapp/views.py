52939c7c0805bbdaca948a48aea4384a20b85d47
parkhan <voftt1@gmail.com>
2022-07-15 12:39:43 +0900

    django CRUD

--- egoing-django/myapp/views.py
+++ egoing-django/myapp/views.py
@@ -11,8 +11,19 @@ topics =[
     {'id': 3, 'title': 'model is park', 'body': 'Model is '},
 ]
 
-def HTMLTemplate(articleTagg):
+def HTMLTemplate(articleTagg, id=None):
     global topics
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+        <li>
+            <form action="/delete/" method="post">
+                <input type="hidden" name="id" value={id}>
+                <input type="submit" value="delete">
+            </form>
+        </li>
+        <li><a href="/update/{id}">update</a></li>
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
@@ -25,7 +36,8 @@ def HTMLTemplate(articleTagg):
         </ol>
         {articleTagg}
         <ul>
-        <li><a href="/create/">create</a></li>
+            <li><a href="/create/">create</a></li>
+            {contextUI}
         </ul>
     </body>
     </html>
@@ -48,7 +60,7 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article, id))
 
 @csrf_exempt
 def create(request):
@@ -70,3 +82,42 @@ def create(request):
         url = '/read/'+str(nextId)
         nextId = nextId + 1
         return redirect(url)
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == 'POST':
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
+
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method =="GET":
+        for topic in topics:
+            if topic['id'] == int(id):
+                selectedTopic={
+                    "title":topic['title'],
+                    "body":topic['body']
+                }
+        article = f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="입력하세요^^" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="이것도 입력하세요~">{selectedTopic['body']}</textarea><p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(HTMLTemplate(article, id))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        for topic in topics:
+            if topic['id'] == int(id):
+                topic['title'] = title
+                topic['body'] = body
+        return redirect(f'/read{id}')
